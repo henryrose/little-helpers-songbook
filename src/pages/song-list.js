9a2c1395c570314d5b83a97fda4446a952de2893
Henry Rose <henryrose@gmail.com>
2022-12-03 12:05:08 -0800

    add songs

--- src/pages/song-list.js
+++ src/pages/song-list.js
@@ -2,11 +2,16 @@ import React from "react"
 import YAMLData from "../../content/songs.yaml"
 
 const SongList = () => (
-  <div style={{ maxWidth: `960px`, margin: `1.45rem` }}>
+  <div style={{ maxWidth: `960px`, margin: `` }}>
     <h1>{YAMLData.title}</h1>
-    <ul>
+    <ul style={{listStyleType: "none", padding: 0}}>
       {YAMLData.content.map((data, index) => {
-        return <li key={`content_item_${index}`}><a href={data.item.songUrl}>{data.item.title}</a></li>
+        return <li 
+                key={`content_item_${index}`} 
+                style={{
+                  padding: `8px 0`
+                }}
+                ><a href={data.item.songUrl}>{data.item.title}</a></li>
       })}
     </ul>
   </div>
