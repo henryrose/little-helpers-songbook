90f2e9b7aec5c02f89f26f0b942cdf7b9878c4d2
Henry Rose <henryrose@gmail.com>
2022-12-03 12:09:23 -0800

    tweaks

--- src/pages/song-list.js
+++ src/pages/song-list.js
@@ -3,13 +3,14 @@ import YAMLData from "../../content/songs.yaml"
 
 const SongList = () => (
   <div style={{ maxWidth: `960px`, margin: `` }}>
-    <h1>{YAMLData.title}</h1>
+    <h2>{YAMLData.title}</h2>
     <ul style={{listStyleType: "none", padding: 0}}>
       {YAMLData.content.map((data, index) => {
         return <li 
                 key={`content_item_${index}`} 
                 style={{
-                  padding: `8px 0`
+                  padding: `8px 0`, 
+                  fontSize: `1.5rem`
                 }}
                 ><a href={data.item.songUrl}>{data.item.title}</a></li>
       })}
