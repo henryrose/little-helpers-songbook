f16eb703c675d5072aa78a7b113e008af2b33dba
Henry Rose <henryrose@gmail.com>
2022-12-03 11:26:01 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `little-helpers-songbook`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
